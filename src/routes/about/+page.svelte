9df64f57930c48f09e2ce8a6b2cc0051b732e277
Sivayogeith Umamaheswaran <sivayogeith@gmail.com>
2025-08-05 17:50:28 +0530

    feat: added image of bhaskar in about page

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,5 +1,6 @@
 <script>
   import uma from "$lib/assets/uma.jpg";
+  import bhaskar from "$lib/assets/bhaskar.jpeg";
 </script>
 
 <h1 class="text-4xl font-bold mb-8 text-center text-primary-700">About the Sellers</h1>
@@ -10,7 +11,7 @@
     <h2 class="text-2xl md:text-3xl font-semibold text-gray-800 mt-4">Umamaheswaran TG</h2>
   </div>
   <div class="bg-white shadow-2xl rounded-2xl p-6 md:p-8 flex flex-col items-center text-center hover:shadow-primary-900 transition-shadow duration-300 md:text-lg">
-    <img class="w-40 h-40 md:w-52 md:h-52 object-cover rounded-full border-4 border-primary-400" src={uma} alt="Uma" />
+    <img class="w-40 h-40 md:w-52 md:h-52 object-cover rounded-full border-4 border-primary-400" src={bhaskar} alt="Uma" />
     <h2 class="text-2xl md:text-3xl font-semibold text-gray-800 mt-4">Bhaskar</h2>
   </div>
 </div>
